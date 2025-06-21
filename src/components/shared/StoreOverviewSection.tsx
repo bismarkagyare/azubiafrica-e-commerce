b101eef28d86eb8f88bb2fecc9293f47b3db0972
Bismark Agyare <bismarkagyare54@gmail.com>
2025-06-21 22:35:31 +0000

    feat: enhance StoreOverviewSection component with responsive image support for desktop, tablet, and mobile views

--- src/components/shared/StoreOverviewSection.tsx
+++ src/components/shared/StoreOverviewSection.tsx
@@ -1,17 +1,23 @@
-import bestGearImg from "@/assets/shared/desktop/image-best-gear.jpg";
+import bestGearImgDesktop from "@/assets/shared/desktop/image-best-gear.jpg";
+import bestGearImgTablet from "@/assets/shared/tablet/image-best-gear.jpg";
+import bestGearImgMobile from "@/assets/shared/mobile/image-best-gear.jpg";
 
 const StoreOverviewSection = () => {
   return (
     <section className="w-full max-w-6xl mx-auto px-4 md:px-6 lg:px-0 py-20 md:py-28 flex flex-col lg:flex-row-reverse items-center gap-12 md:gap-8">
-      <div className="flex-1 flex justify-center items-center w-full max-w-lg lg:max-w-none order-1 lg:order-none">
-        <img
-          src={bestGearImg}
-          alt="Person enjoying high-end audio gear"
-          className="rounded-xl w-full h-auto object-cover object-center"
-          draggable={false}
-        />
+      
+      <div className="flex-1 flex justify-center items-center w-full max-w-lg md:max-w-none lg:max-w-none order-1 lg:order-none">
+        <picture className="w-full">
+          <source media="(min-width:1024px)" srcSet={bestGearImgDesktop} />
+          <source media="(min-width:640px)" srcSet={bestGearImgTablet} />
+          <img
+            src={bestGearImgMobile}
+            alt="Person enjoying high-end audio gear"
+            className="rounded-xl w-full h-auto object-cover object-center"
+            draggable={false}
+          />
+        </picture>
       </div>
-
 
       <div className="flex-1 flex flex-col justify-center items-center lg:items-start text-center lg:text-left order-2 lg:order-none">
         <h2 className="text-3xl md:text-4xl font-bold mb-8 text-neutral-900 leading-tight">
