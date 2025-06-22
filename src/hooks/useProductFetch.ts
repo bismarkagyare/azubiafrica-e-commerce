eefc24cf8af9d8cd4698f5c51686b5a27e716d06
Bismark Agyare <bismarkagyare54@gmail.com>
2025-06-22 14:14:54 +0000

    feat: implement custom hook useProducts for fetching product data

---
+++ src/hooks/useProductFetch.ts
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { api } from "@/services/axios";
+import type { Product } from "@/types/product";
+import { queryKeys } from "@/utils/queryKeys";
+
+type ProductsJson = { products: Product[] };
+
+const fetchProducts = async (): Promise<Product[]> => {
+  const { data } = await api.get<ProductsJson>("products.json");
+  return data.products;
+};
+
+export const useProducts = () =>
+  useQuery({
+    queryKey: [queryKeys.PRODUCTS],
+    queryFn: fetchProducts,
+    staleTime: 5 * 60 * 1000,
+  });
