c0af13962842912e015ec07d192c8731b642038d
Bismark Agyare <bismarkagyare54@gmail.com>
2025-06-22 17:27:23 +0000

    feat: implement routing for product details

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,28 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./layout";
 import Home from "./pages/home";
+import HeadphonesCategory from "./pages/headphones";
+import SpeakersCategory from "./pages/speakers";
+import EarphonesCategory from "./pages/earphones";
+import ProductDetails from "./pages/product-details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <Router>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/headphones" element={<HeadphonesCategory />} />
+            <Route path="/speakers" element={<SpeakersCategory />} />
+            <Route path="/earphones" element={<EarphonesCategory />} />
+            <Route path=":category/:productSlug" element={<ProductDetails />} />
+          </Route>
+        </Routes>
+      </QueryClientProvider>
     </Router>
   );
 }
