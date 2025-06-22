fd6938e9b2043306169d4e1cab3faa4b806aa24f
Bismark Agyare <bismarkagyare54@gmail.com>
2025-06-22 17:25:45 +0000

    feat: add related products section

---
+++ src/pages/product-details/RelatedProducts.tsx
@@ -0,0 +1,60 @@
+import type { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import type { RelatedProduct } from "@/types/product";
+
+type RelatedProductsSectionProps = {
+  others: RelatedProduct[];
+  category: string;
+};
+
+const RelatedProducts: FC<RelatedProductsSectionProps> = ({
+  others,
+  category,
+}) => {
+  const navigate = useNavigate();
+
+  return (
+    <section className="w-full max-w-6xl mx-auto px-4 md:px-6 py-8 md:py-16">
+      <h2 className="text-2xl md:text-3xl font-bold uppercase text-center mb-12 tracking-wide">
+        You May Also Like
+      </h2>
+
+      <div className="flex flex-col md:flex-row gap-12 md:gap-6 lg:gap-8 items-center justify-center">
+        {others.map((item) => (
+          <div
+            key={item.slug}
+            className="flex flex-col items-center text-center w-full md:w-1/3 max-w-xs md:max-w-none"
+          >
+
+            <div className="w-full rounded-xl  md:aspect-[3/4] lg:aspect-square mb-8 overflow-hidden">
+              <picture className="block w-full h-full">
+                <source media="(min-width:1024px)" srcSet={item.image.desktop} />
+                <source media="(min-width:640px)" srcSet={item.image.tablet} />
+                <img
+                  src={item.image.mobile}
+                  alt={item.name}
+                  className="w-full h-full object-cover"
+                  draggable={false}
+                />
+              </picture>
+            </div>
+
+            <h3 className="text-lg md:text-xl font-bold uppercase tracking-wide mb-6">
+              {item.name}
+            </h3>
+
+            <button
+              onClick={() => navigate(`/${category}/${item.slug}`)}
+              className="bg-primary-orangeish hover:bg-primary-orangeish/80 text-white font-bold uppercase tracking-widest px-8 py-4 text-base transition-colors duration-150 cursor-pointer"
+              type="button"
+            >
+              See Product
+            </button>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default RelatedProducts;
