42e4c5090d02237c2409b5c8531434869a27c66b
Bismark Agyare <bismarkagyare54@gmail.com>
2025-06-23 02:18:06 +0000

    chore: move assets into public folder

--- src/hooks/useProductFetch.ts
+++ src/hooks/useProductFetch.ts
@@ -1,3 +1,21 @@
+// import { useQuery } from "@tanstack/react-query";
+// import { api } from "@/services/axios";
+// import type { Product } from "@/types/product";
+// import { queryKeys } from "@/utils/queryKeys";
+
+// type ProductsJson = { products: Product[] };
+
+// const fetchProducts = async (): Promise<Product[]> => {
+//   const { data } = await api.get<ProductsJson>("products.json");
+//   return data.products;
+// };
+
+// export const useProducts = () =>
+//   useQuery({
+//     queryKey: [queryKeys.PRODUCTS],
+//     queryFn: fetchProducts,
+//   });
+
 import { useQuery } from "@tanstack/react-query";
 import { api } from "@/services/axios";
 import type { Product } from "@/types/product";
@@ -6,7 +24,8 @@ import { queryKeys } from "@/utils/queryKeys";
 type ProductsJson = { products: Product[] };
 
 const fetchProducts = async (): Promise<Product[]> => {
-  const { data } = await api.get<ProductsJson>("products.json");
+  const url = `${import.meta.env.BASE_URL}products.json`;
+  const { data } = await api.get<ProductsJson>(url);
   return data.products;
 };
 
